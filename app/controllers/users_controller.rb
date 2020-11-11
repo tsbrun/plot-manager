df63b2952530f1e02198a2ae15a4a6d881ff5d11
tsbrun <tsbrun@icloud.com>
2020-11-11 13:12:03 -0500

    Build routes and views for login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
 
-    
+    get '/login' do
+        erb :"users/login"
+    end
+
+    post '/login' do
+        binding.pry
+        @user = User.find_by(name: params["name"], email: params["email"])
+        if @user && @user.authenticate(params["password"])
+            session[:user_id] = @user.id 
+            redirect "/users/#{@user.id}"
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/users/:id' do
+        erb :"users/show"
+    end
 end
