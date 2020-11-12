55241e875920b35c669878fe1382a8f20fdffd51
tsbrun <tsbrun@icloud.com>
2020-11-11 21:43:13 -0500

    Implement READ functionality for characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,6 +1,9 @@
 class CharactersController < ApplicationController
 
-    get '/characters' do
-        "Here are all your plot's characters."
+    get '/plots/:id/characters' do
+        user = current_user(session)
+        @plot = user.plots.find(params[:id].to_i)
+        @characters = @plot.characters
+        erb :"characters/index"
     end
 end
