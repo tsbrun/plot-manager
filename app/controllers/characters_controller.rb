9ecef1a760b2f75022eb5fec5752abecae99ca58
tsbrun <tsbrun@icloud.com>
2020-11-12 12:20:51 -0500

    Build 'show route and view for characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -7,16 +7,21 @@ class CharactersController < ApplicationController
         erb :"characters/index"
     end
 
-    get '/plots/:id/characters/new' do
-        binding.pry
-        erb :"characters/new"
+    get '/characters/:id' do
+        @character = Character.find(params[:id])
+        erb :"characters/show"
     end
 
-    post '/plots/:id/characters' do
-        binding.pry
-        character = Character.create(name: params["name"], role: params["role"], description: params["description"])
-        plot = Plot.find(params[:id])
-        plot.characters << character
-        redirect '/plots/:id/characters'
-    end
+    # get '/plots/:id/characters/new' do
+    #     binding.pry
+    #     erb :"characters/new"
+    # end
+
+    # post '/plots/:id/characters' do
+    #     binding.pry
+    #     character = Character.create(name: params["name"], role: params["role"], description: params["description"])
+    #     plot = Plot.find(params[:id])
+    #     plot.characters << character
+    #     redirect '/plots/:id/characters'
+    # end
 end
