3bd1d664d629e19f149c0753844e9ee5539784ef
tsbrun <tsbrun@icloud.com>
2020-11-12 12:10:49 -0500

    Add new character to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 stan = User.create(name: "Stan Lee", email: "stan.lee@marvel.com", password: "dccouldnever")
 homecoming = Plot.create(title: "Spider-Man: Homecoming", summary: "Best Spider-Man movie ever.")
 spidey = Character.create(name: "Peter Parker", role: "Main Protagonist", description: "Nerdy, witty, awkward, likes wearing spandex onesies.")
+mj = Character.create(name: "MJ", role: "Main Love Interest", description: "Cool, sarcastic, looks like Zendaya.")
 
 stan.plots << homecoming
 homecoming.characters << spidey
+homecoming.characters << mj
 
 # , characters: "Spider-Man", summary: "Best Spider-Man movie ever."
 # , role: "Main Protagonist", description: "Nerdy, witty, awkward, likes wearing spandex onesies."
