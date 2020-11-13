6dcb134db160850ac7b15ced973536607cb3184c
tsbrun <tsbrun@icloud.com>
2020-11-12 20:42:59 -0500

    Add new-user validation failure message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     has_many :plots 
     has_many :characters, through: :plots
+    validates :name, :email, :password, presence: true
     has_secure_password
 end
