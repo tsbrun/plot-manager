0cff59acf7303a2fc4c66a922db3409a71eb12e0
tsbrun <tsbrun@icloud.com>
2020-11-13 20:31:03 -0500

    Delete comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,3 @@ mj = Character.create(name: "MJ", role: "Main Love Interest", description: "Cool
 stan.plots << homecoming
 homecoming.characters << spidey
 homecoming.characters << mj
-
-# , characters: "Spider-Man", summary: "Best Spider-Man movie ever."
-# , role: "Main Protagonist", description: "Nerdy, witty, awkward, likes wearing spandex onesies."
-
-# since each instance of Plot has_many :characters, you're not supposed to fill the characters attribute manually. 
-# You need to do so through ActiveRecord (e.g., homecoming.characters << spidey)
-
-# delete column in migration "characters" from plots table
