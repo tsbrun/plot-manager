e5785d71a0699caec35582a23367bf26b265f1b7
tsbrun <tsbrun@icloud.com>
2020-11-13 21:19:20 -0500

    Implement email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
     has_many :plots 
     has_many :characters, through: :plots
     validates :name, :email, :password, presence: true
-    validates :email, uniqueness: true 
+    validates :email, 
+    format: { with: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/  },
+            uniqueness: { case_sensitive: false }
     has_secure_password
 end
